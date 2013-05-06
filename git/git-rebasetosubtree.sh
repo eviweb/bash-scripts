d1f63c6bdf0983d2f1237525af8a60c4f77c52a7
Eric Villard <dev@eviweb.fr>
2013-05-06 15:27:34 +0200

    fix folder leading / issue, remove foregotten echo

--- git/git-rebasetosubtree.sh
+++ git/git-rebasetosubtree.sh
@@ -1,11 +1,12 @@
 #! /bin/bash
+# this script will help you to rebase a repository to a subtree of it
 # check for provided directory
 if [ -z "$1" ]
 then
     echo "Need the directory to get content rebased !"
     exit 1
 fi
-DIR=$1
+DIR=`echo $1 | sed 's/\/$//'`
 BRANCH=$(git rev-parse --abbrev-ref HEAD)
 NEWBRANCH="new-$BRANCH-rebased"
 
@@ -19,16 +20,13 @@ fi
 # create target tree using provided directory subtree
 HASH=$(git ls-tree $BRANCH $1/ | sed -e "s/$1\///" | git mktree)
 HASH=$(echo "remove $1 from repository tree" | git commit-tree $HASH)
-echo $HASH
 # check for existing target branch
 git show-ref --verify --quiet "refs/heads/$NEWBRANCH"
 if [[ $? != 0 ]]
 then
-echo 'not exists'
     # checkout the target tree in a new branch
     git checkout -b $NEWBRANCH $HASH
 else
-echo 'exists'
     # rebase the branch
     git checkout $NEWBRANCH && git rebase -p $HASH
 fi
