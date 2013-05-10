d33d8b1007238f4e101a6e8908582d130b06014a
Eric Villard <dev@eviweb.fr>
2013-05-10 16:28:21 +0200

    first release ruby installer

---
+++ ruby/install_ruby.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+[[ -n `which ruby` ]] && exit 0
+[[ -z `which curl` ]] && sudo apt-get update && sudo apt-get install curl
+BACKUP_DIR=`pwd`
+cd $HOME
+\curl -L https://get.rvm.io | bash -s stable
+source $HOME/.rvm/scripts/rvm
+rvm requirements
+rvm install 1.9.3
+rvm use 1.9.3 --default
+rvm rubygems current
+echo "Don't forget to run 'source ~/.bashrc' to reload your shell config'
